b7ab8bb8fe91db38e5b96170ada499a692e60045
Wb0214 <Wb0214@gmail.com>
2024-12-24 14:07:06 +0800

    [feat] 原始主頁 和 軌跡 完成

---
+++ app/src/main/java/com/example/test_c/Fragment3.java
@@ -0,0 +1,33 @@
+package com.example.test_c;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class Fragment3 extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_3, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view,@Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+    }
+}
